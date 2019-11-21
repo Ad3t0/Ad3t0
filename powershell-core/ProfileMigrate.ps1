350d6df7581db24e217554a7765d07ec44d34087
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2019-11-21 00:24:28 -0700

    Update ProfileMigrate.ps1

--- powershell-core/ProfileMigrate.ps1
+++ powershell-core/ProfileMigrate.ps1
@@ -3,7 +3,7 @@
 #	Creator:	Ad3t0	                    #
 #	Date:		05/22/2019             	    #
 #############################################
-$ver = "1.1.6"
+$ver = "1.1.7"
 $text1 = @'
      _       _ _____ _    ___
     / \   __| |___ /| |_ / _ \
@@ -17,11 +17,8 @@ Write-Host $text1
 Write-Host $text2 -ForegroundColor Yellow
 Write-Host $text3 -ForegroundColor Gray -NoNewline
 Write-Host $ver -ForegroundColor Green
-
-
 $currentUser = (Get-WmiObject -Class Win32_Process -Filter 'Name="explorer.exe"').GetOwner().User
 $currentUserProfile = "C:\Users\$($currentUser)"
-
 $fsizeDocuments = "{0:N2} MB" -f ((Get-ChildItem "$($currentUserProfile)\Documents" -Recurse | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue).Sum / 1MB)
 $fsizePictures = "{0:N2} MB" -f ((Get-ChildItem "$($currentUserProfile)\Pictures" -Recurse | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue).Sum / 1MB)
 $fsizeDesktop = "{0:N2} MB" -f ((Get-ChildItem "$($currentUserProfile)\Desktop" -Recurse | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue).Sum / 1MB)
@@ -31,6 +28,8 @@ Write-Host "$($currentUserProfile)\Documents | $fsizeDocuments" -ForegroundColor
 Write-Host "$($currentUserProfile)\Pictures  | $fsizePictures" -ForegroundColor yellow
 Write-Host "$($currentUserProfile)\Desktop   | $fsizeDesktop" -ForegroundColor yellow
 Write-Host
+$currentUser = (Get-WmiObject -Class Win32_Process -Filter 'Name="explorer.exe"').GetOwner().User
+$currentUserProfile = "C:\Users\$($currentUser)"
 while ($migrateToPath -like '*Documents*' -or $migrateToPath -like '*Pictures*' -or $migrateToPath -like '*Desktop*' -or $migrateToPath -like '') {
 	Write-Host "Type the full path to migrate the data to"
 	Write-Host "Example: C:\Users\Admin\Downloads"
@@ -42,7 +41,7 @@ while ($migrateToPath -like '*Documents*' -or $migrateToPath -like '*Pictures*' 
 		Write-Host
 		Read-Host "Press ENTER to continue"
 	}
-}if ($migrateToPath -like '*_DATAPM*')
+} if ($migrateToPath -like '*_DATAPM*')
 { while ($confirmRCM -ne "n" -and $confirmRCM -ne "y")
 	{
 		Write-Host "Data discovered at $($migrateToPath)" -ForegroundColor green
@@ -55,6 +54,13 @@ while ($migrateToPath -like '*Documents*' -or $migrateToPath -like '*Pictures*' 
 		robocopy "$($migrateToPath)\Pictures\" "$($currentUserProfile)\Pictures\" /s /Mov /xf *.pst desktop.ini
 		robocopy "$($migrateToPath)\Desktop\" "$($currentUserProfile)\Desktop\" /s /Mov /xf *.pst desktop.ini
 		robocopy "$($migrateToPath)\Favorites\" "$($currentUserProfile)\Favorites\" /s /Mov /xf *.pst desktop.ini
+		if (Test-Path -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Firefox.lnk")
+		{
+			. "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Firefox.lnk"
+			Sleep 3
+			$firefoxProfile = Get-ChildItem -Path "$($currentUserProfile)\AppData\Roaming\Mozilla\Firefox\Profiles\" | Where-Object { $_.PSIsContainer } | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+			Copy-Item -Path "$($migrateToPath)\Browsers\Firefox\places.sqlite" -Destination "$($currentUserProfile)\AppData\Roaming\Mozilla\Firefox\Profiles\$($firefoxProfile.Name)" -Force
+		}
 	}
 } else
 { while ($confirmRCB -ne "n" -and $confirmRCB -ne "y")
@@ -67,12 +73,22 @@ while ($migrateToPath -like '*Documents*' -or $migrateToPath -like '*Pictures*' 
 			New-Item -Path "$($migrateToPath)\$($env:USERNAME)_DATAPM\Pictures" -ItemType "directory"
 			New-Item -Path "$($migrateToPath)\$($env:USERNAME)_DATAPM\Desktop" -ItemType "directory"
 			New-Item -Path "$($migrateToPath)\$($env:USERNAME)_DATAPM\Favorites" -ItemType "directory"
+			New-Item -Path "$($migrateToPath)\$($env:USERNAME)_DATAPM\Browsers\Firefox" -ItemType "directory"
+			New-Item -Path "$($migrateToPath)\$($env:USERNAME)_DATAPM\Browsers\Google Chrome" -ItemType "directory"
 		} robocopy "$($currentUserProfile)\Documents" "$($migrateToPath)\$($env:USERNAME)_DATAPM\Documents" /s /xf *.pst *.lnk desktop.ini
 		robocopy "$($currentUserProfile)\Pictures" "$($migrateToPath)\$($env:USERNAME)_DATAPM\Pictures" /s /xf *.pst *.lnk desktop.ini
 		robocopy "$($currentUserProfile)\Desktop" "$($migrateToPath)\$($env:USERNAME)_DATAPM\Desktop" /s /xf *.pst *.lnk desktop.ini
 		robocopy "$($currentUserProfile)\Favorites" "$($migrateToPath)\$($env:USERNAME)_DATAPM\Favorites" /s /xf *.pst desktop.ini
+		if (Test-Path -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Firefox.lnk")
+		{
+			. "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Firefox.lnk"
+			Sleep 3
+			$firefoxProfile = Get-ChildItem -Path "$($currentUserProfile)\AppData\Roaming\Mozilla\Firefox\Profiles\" | Where-Object { $_.PSIsContainer } | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+			Copy-Item -Path "$($currentUserProfile)\AppData\Roaming\Mozilla\Firefox\Profiles\$($firefoxProfile.Name)\places.sqlite" -Destination "$($migrateToPath)\$($env:USERNAME)_DATAPM\Browsers\Firefox\" -Force
+		}
 	}
-} if ($confirmRCB -eq "n" -or $confirmRCM -eq "n")
+}
+if ($confirmRCB -eq "n" -or $confirmRCM -eq "n")
 { Read-Host "Press ENTER to exit"
 } else
 { Write-Host "Complete! Credential Manager will open next." -ForegroundColor green
