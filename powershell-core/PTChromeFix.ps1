b6f64bba1163bdb955767e2f8390cbd31c534acd
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2019-12-17 11:39:42 -0700

    Update PTChromeFix.ps1

--- powershell-core/PTChromeFix.ps1
+++ powershell-core/PTChromeFix.ps1
@@ -1,3 +1,6 @@
+Stop-Process -Name Chrome -Force -ErrorAction SilentlyContinue
+."C:\Program Files\Confer\Uninstall.exe" /uninstall
 New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Force
 $value1 = '~ WIN8RTM'
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -Value $value1
+Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
