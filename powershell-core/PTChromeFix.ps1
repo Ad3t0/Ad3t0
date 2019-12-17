a3f9cf8892df775ee87076339b23a8e4d0febbd1
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2019-12-17 10:54:39 -0700

    Update PTChromeFix.ps1

--- powershell-core/PTChromeFix.ps1
+++ powershell-core/PTChromeFix.ps1
@@ -1,2 +1,3 @@
 New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" –Force
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -Value '~ WIN8RTM'
+$value1 = '~ WIN8RTM'
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -Value $value1
