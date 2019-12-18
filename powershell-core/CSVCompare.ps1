803444928327dc26e8ea78afdc6a09ae0e55ffb5
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2019-12-18 09:09:40 -0700

    Update CSVCompare.ps1

--- powershell-core/CSVCompare.ps1
+++ powershell-core/CSVCompare.ps1
@@ -1,20 +1,15 @@
 $file1 = Import-Csv -Path "C:\Users\Administrator\Downloads\Mock Up - Opportunities.csv"
 $file2 = Import-Csv -Path "C:\Users\Administrator\Downloads\Mock Up - All Data.csv"
-$test = Compare-Object $file1 $file2 -Property Company -PassThru #| Get-Unique
+$test = Compare-Object $file1 $file2 -Property Company -PassThru
 $dupes = Compare-Object $file1 $file2 -Property Company -PassThru -IncludeEqual -ExcludeDifferent
-$dupes = $dupes | ForEach-Object {$_.Company}
-$test = $test | Where-Object {$_.SideIndicator -eq "=>"}
-
-foreach($company in $test)
-{
-    $company.Company = $company.Company.Replace("-","").Replace(" ","")
+$dupes = $dupes | ForEach-Object { $_.Company }
+$test = $test | Where-Object { $_.SideIndicator -eq "=>" }
+foreach ($company in $test) {
+    $company.Company = $company.Company.Replace("-", "").Replace(" ", "")
 }
 foreach ($dupe in $dupes) {
-    
-    $dupe = $dupe.Replace("-","").Replace(" ","")
-
-    $test = $test | Where-Object {$_.Company -ne "*$($dupe)*"}
+    $dupe = $dupe.Replace("-", "").Replace(" ", "")
+    $test = $test | Where-Object { $_.Company -notlike "*$($dupe)*" }
 }
-
-#$test = $test.PSObject.properties.remove('SideIndicator')
-#$test | Export-Csv -NoTypeInformation -Path "C:\Users\Administrator\Downloads\uni.csv"
+$test.PSObject.properties.remove('SideIndicator')
+$test | Export-Csv -NoTypeInformation -Path "C:\Users\Administrator\Downloads\uni.csv"
