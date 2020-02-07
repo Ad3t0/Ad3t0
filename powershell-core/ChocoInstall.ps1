8345b6504eaa6708a5bcceef95453b51da79ebb9
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2020-02-07 11:09:18 -0700

    Update ChocoInstall.ps1

--- powershell-core/ChocoInstall.ps1
+++ powershell-core/ChocoInstall.ps1
@@ -17,6 +17,7 @@ Write-Host $text1
 Write-Host $text2 -ForegroundColor Yellow
 Write-Host $text3 -ForegroundColor Gray -NoNewline
 Write-Host $ver -ForegroundColor Green
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n=allowGlobalConfirmation
 choco feature disable -n=checksumFiles
