15b1b40e819aeef1fba0270627407cf8a99ef5e7
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2020-02-11 12:24:13 -0700

    Update OpenVPN.ps1

--- powershell-core/OpenVPN.ps1
+++ powershell-core/OpenVPN.ps1
@@ -43,14 +43,29 @@ Write-Host $text1
 Write-Host $text2 -ForegroundColor Yellow
 Write-Host $text3 -ForegroundColor Gray -NoNewline
 Write-Host $ver -ForegroundColor Green
-if (!(Test-Path -Path "C:\ProgramData\chocolatey\choco.exe")) {
-	Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-	choco feature enable -n=allowGlobalConfirmation
-	choco feature disable -n=checksumFiles
+
+if ([System.Environment]::OSVersion.Version.Major -ge 6.2) {
+	if (!(Test-Path -Path "C:\Program Files\OpenVPN\bin\openvpn.exe") {
+		$url = "https://swupdate.openvpn.org/community/releases/openvpn-install-2.4.8-I602-Win10.exe"
+		$output = "$($env:TEMP)\openvpn-install-2.4.8-I602-Win10.exe"
+		Invoke-WebRequest -Uri $url -OutFile $output
+	}
+
+	
 }
-if (!(Test-Path -Path "$($env:ProgramFiles)\OpenVPN\")) {
-	choco install openvpn
+else {
+	if (!(Test-Path -Path "C:\ProgramData\chocolatey\choco.exe")) {
+		Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+		choco feature enable -n=allowGlobalConfirmation
+		choco feature disable -n=checksumFiles
+	}
+	if (!(Test-Path -Path "C:\Program Files\OpenVPN\bin\openvpn.exe")) {
+		choco install openvpn
+	}
+
 }
+
+
 $decURL = Decrypt-String -Encrypted $encURL -Passphrase $pass
 Invoke-WebRequest -Uri $decURL -UseBasicParsing
 $webClient = New-Object System.Net.WebClient
