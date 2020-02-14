0a4dbe31cd4bc0da2c710dbdcea2252f34c5210e
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2020-02-14 09:21:06 -0700

    Update OpenVPN_InstallOnly.ps1

--- powershell-core/OpenVPN_InstallOnly.ps1
+++ powershell-core/OpenVPN_InstallOnly.ps1
@@ -4,6 +4,7 @@ if ([System.Environment]::OSVersion.Version.Major -ge 6.2) {
 		$output = "$($env:TEMP)\openvpn-install-2.4.8-I602-Win10.exe"
 		Invoke-WebRequest -Uri $url -OutFile $output
 		.$output /S /SELECT_SHORTCUTS=0
+		Wait-Process -Name "openvpn-install-2.4.8-I602-Win10"
 	}
 }
 else {
@@ -16,12 +17,12 @@ else {
 		choco install openvpn
 	}
 }
-$url = "ftp://ad3t0.ddns.net/VPN/$($DOMAIN).ovpn"
-$output = "C:\Program Files\OpenVPN\config\$($DOMAIN).ovpn"
+$url = "ftp://ad3t0.ddns.net/VPN/$($DOMAIN.Domain).ovpn"
+$output = "C:\Program Files\OpenVPN\config\$($DOMAIN.Domain).ovpn"
 Invoke-WebRequest -Uri $url -OutFile $output
 $DOMAIN = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters -Name "Domain"
 $DOMAINSplit = $DOMAIN.Domain.Split(".").ToUpper()
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OpenVPN-GUI" -Value "C:\Program Files\OpenVPN\bin\openvpn-gui.exe"
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OpenVPN-GUI" -Value "C:\Program Files\OpenVPN\bin\openvpn-gui.exe" -ErrorAction SilentlyContinue
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$([environment]::GetFolderPath("Desktop"))\Connect VPN to $($DOMAINSplit[0]).lnk")
 $Shortcut.TargetPath = """C:\Program Files\OpenVPN\bin\openvpn-gui.exe"""
