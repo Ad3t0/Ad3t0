447eb41356588c9dfafcd19be257f9f18eab130d
ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2020-02-18 09:43:32 -0700

    update

---
+++ powershell-core/OpenVPN_Single.ps1
@@ -0,0 +1,25 @@
+function Decrypt-String ($Encrypted, $Passphrase, $salt = "Ad3t049866", $init = "Ad3t0PASS") {
+	if ($Encrypted -is [string]) {
+		$Encrypted = [Convert]::FromBase64String($Encrypted)
+	}
+	$r = New-Object System.Security.Cryptography.RijndaelManaged
+	$pass = [Text.Encoding]::UTF8.GetBytes($Passphrase)
+	$salt = [Text.Encoding]::UTF8.GetBytes($salt)
+	$r.Key = (New-Object Security.Cryptography.PasswordDeriveBytes $pass, $salt, "SHA1", 5).GetBytes(32)
+	$r.IV = (New-Object Security.Cryptography.SHA1Managed).ComputeHash([Text.Encoding]::UTF8.GetBytes($init))[0..15]
+	$d = $r.CreateDecryptor()
+	$ms = New-Object IO.MemoryStream @(, $Encrypted)
+	$cs = New-Object Security.Cryptography.CryptoStream $ms, $d, "Read"
+	$sr = New-Object IO.StreamReader $cs
+	Write-Output $sr.ReadToEnd()
+	$sr.Close()
+	$cs.Close()
+	$ms.Close()
+	$r.Clear()
+}
+$encURL = 'x6kxt3P2xDXHXDBdbGUCroGC6g3is8SL6Mlxn4GTL9SXuOj68dtSrNzvD5MtrJPaCAWbMRQB+UNJzAnxNd2CYLQMpgp/2xzpiGGCeWzrWSasG5KH93lUmL4nMLmRBOk10CVavyWGjls+7xnayyJUmOOg7wcJdWytfN938LBpjugjdzDTrPTnBP26jua3agOCnzrH5kHp5PZ0MuWF+5xXNw=='
+$pass = Read-Host "Password"
+$decURL = Decrypt-String -Encrypted $encURL -Passphrase $pass
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$Script = Invoke-RestMethod "$($decURL)" -Headers @{"Accept" = "application/vnd.github.v3.raw" }
+Invoke-Expression $Script
