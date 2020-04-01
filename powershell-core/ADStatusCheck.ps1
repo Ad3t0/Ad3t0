be01fbc745cb8981852007ea5d814242868b7843
ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2020-04-01 13:18:51 -0600

    Update ADStatusCheck.ps1

--- powershell-core/ADStatusCheck.ps1
+++ powershell-core/ADStatusCheck.ps1
@@ -1,7 +1,4 @@
 $scr = ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/chrisdee/Scripts/master/PowerShell/Working/AD/GetADForestHealthStatusReport.ps1'))
-
-
 $scr | Out-File -FilePath $env:TEMP\GetADForestHealthStatusReport.ps1
-
-
 .$env:TEMP\GetADForestHealthStatusReport.ps1 -ReportFile
+Start-Process "$($env:USERPROFILE)\forest_health_report_$($env:USERDNSDOMAIN).html"
