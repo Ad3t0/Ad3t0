bc583b6f5e8a6a3b898c1baebd8f4cbbbf69d30e
ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2020-04-06 12:20:50 -0600

    update

--- powershell-core/CleanerWindows10.ps1
+++ powershell-core/CleanerWindows10.ps1
@@ -1,114 +1,60 @@
-$ver = "3.0.0"
-if ((Get-WmiObject win32_operatingsystem).Name -notlike "*Windows 10*") {
- Write-Warning "Operating system is not Windows 10..."
-	Read-Host "The script will now exit..."
-	exit
-} $systemmodel = wmic computersystem get model /VALUE
+$ver = "3.0.1"
+$ErrorActionPreference = "SilentlyContinue"
+$systemmodel = wmic computersystem get model /VALUE
 $systemmodel = $systemmodel -replace ('Model=', '')
 $currentversion = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name "ReleaseId" -ErrorAction SilentlyContinue
 $productname = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name "ProductName" -ErrorAction SilentlyContinue
-function header {
-	Write-Host $ver -ForegroundColor Green
-	Write-Host $text
-	Write-Host " System Model: " -ForegroundColor yellow -NoNewline
-	Write-Host $systemmodel -ForegroundColor white
-	Write-Host " Operating System: " -ForegroundColor yellow -NoNewline
-	Write-Host $productname.ProductName $currentversion.ReleaseId -ForegroundColor white
-	Write-Host " PC Name: " -ForegroundColor yellow -NoNewline
-	Write-Host $env:COMPUTERNAME -ForegroundColor white
-	Write-Host " Username: " -ForegroundColor yellow -NoNewline
-	Write-Host $env:USERNAME -ForegroundColor white
-	Write-Host " Domain: " -ForegroundColor yellow -NoNewline
-	Write-Host $env:USERDNSDOMAIN -ForegroundColor white
-	Write-Host
-} header
-New-Item -Path $env:TEMP -Name "powershell-bin" -ItemType "directory" -Force > $null 2>&1
-Set-Location "$($env:TEMP)\powershell-bin"
- while ($initialsetting -ne "1" -and $initialsetting -ne "2") {
- Clear-Host
-	header
-	if (([string]::IsNullOrEmpty($initialsetting)) -ne $true) {
-		if ($initialsetting -ne "1" -and $initialsetting -ne "2") {
-			Write-Warning "Invalid option"
-		}
-	}
-	Write-Host "----------------------------------------"
-	Write-Host
-	Write-Host " 1 - Basic"
-	Write-Host " 2 - Advanced"
-	Write-Host
-	$initialsetting = Read-Host -Prompt "Input option"
-} while ($confirmationpowersch -ne "n" -and $confirmationpowersch -ne "y") {
+Write-Output $ver -ForegroundColor Green
+Write-Output $text
+Write-Output " System Model: " -ForegroundColor yellow -NoNewline
+Write-Output $systemmodel -ForegroundColor white
+Write-Output " Operating System: " -ForegroundColor yellow -NoNewline
+Write-Output $productname.ProductName $currentversion.ReleaseId -ForegroundColor white
+Write-Output " PC Name: " -ForegroundColor yellow -NoNewline
+Write-Output $env:COMPUTERNAME -ForegroundColor white
+Write-Output " Username: " -ForegroundColor yellow -NoNewline
+Write-Output $env:USERNAME -ForegroundColor white
+Write-Output " Domain: " -ForegroundColor yellow -NoNewline
+Write-Output $env:USERDNSDOMAIN -ForegroundColor white
+Write-Output
+while ($confirmationpowersch -ne "n" -and $confirmationpowersch -ne "y") {
  $confirmationpowersch = Read-Host "Set PowerScheme to maximum performance? [y/n]"
-} while ($confirmationstartmenu -ne "n" -and $confirmationstartmenu -ne "y") {
- $confirmationstartmenu = Read-Host "Unpin all StartMenu icons? [y/n]"
-	while ($confirmationappremoval -ne "n" -and $confirmationappremoval -ne "y") {
-		$confirmationappremoval = Read-Host "Remove all Windows Store apps except the Calculator, Photos, StickyNotes, and the Windows Store? [y/n]"
-	}
-} while ($confirmationchocoinstall -ne "n" -and $confirmationchocoinstall -ne "y") {
+}
+while ($confirmationappremoval -ne "n" -and $confirmationappremoval -ne "y") {
+	$confirmationappremoval = Read-Host "Remove all Windows Store apps except the Calculator, Photos, StickyNotes, and the Windows Store? [y/n]"
+}
+while ($confirmationchocoinstall -ne "n" -and $confirmationchocoinstall -ne "y") {
  $confirmationchocoinstall = Read-Host "Install Chocolatey and choose packages? [y/n]"
-} if ($initialsetting -eq "2") {
- while ($confirmationonedrive -ne "n" -and $confirmationonedrive -ne "y") {
-		$confirmationonedrive = Read-Host "Remove all traces of OneDrive? [y/n]"
-	}
-	while ($confirmationwallpaperq -ne "n" -and $confirmationwallpaperq -ne "y") {
-		$confirmationwallpaperq = Read-Host "Increase desktop wallpaper compression to max quality? [y/n]"
-	}
-	while ($confirmationshowfileex -ne "n" -and $confirmationshowfileex -ne "y") {
-		$confirmationshowfileex = Read-Host "Show file extension in File Explorer? [y/n]"
-	}
-	while ($confirmationshowhiddenfiles -ne "n" -and $confirmationshowhiddenfiles -ne "y") {
-		$confirmationshowhiddenfiles = Read-Host "Show hidden files in File Explorer? [y/n]"
-	}
-	while ($confirmationrdp -ne "n" -and $confirmationrdp -ne "y") {
-		$confirmationrdp = Read-Host "Enable Allow Remote Desktop Connections? [y/n]"
-	}
-	while ($confirmationwol -ne "n" -and $confirmationwol -ne "y") {
-		$confirmationwol = Read-Host "Enable Allow Wake On LAN? [y/n]"
-	}
-	while ($confirmationhostsadb -ne "n" -and $confirmationhostsadb -ne "y") {
-		$confirmationhostsadb = Read-Host "Download MVPS hosts for system wide ad blocking? [y/n]"
-	}
-} if ($confirmationchocoinstall -eq "y") {
- Write-Host
-	Write-Host "A .txt file containing the Chocolatey packages to be installed will now open"
-	Write-Host "edit, save and close the file separating each package name with a semicolon"
-	Write-Host
-	Read-Host "Press ENTER to open the chocolist.txt file"
-	notepad.exe "$($env:TEMP)\powershell-bin\chocolist.txt"
-	Read-Host "Press ENTER to continue after the chocolist.txt file has been saved"
-} $chocolist = [IO.File]::ReadAllText("$($env:TEMP)\powershell-bin\chocolist.txt")
-Write-Host
-Write-Host "Maximum PowerScheme: [$($confirmationpowersch)]"
-Write-Host "Unpin All StartMenu Icons: [$($confirmationstartmenu)]"
-Write-Host "App Removal: [$($confirmationappremoval)]"
-Write-Host "Choco install: [$($confirmationchocoinstall)]"
-if ($initialsetting -eq "2") {
- Write-Host "OneDrive Removal: [$($confirmationonedrive)]"
-	Write-Host "Wallpaper Max Quality: [$($confirmationwallpaperq)]"
-	Write-Host "Show File Extensions: [$($confirmationshowfileex)]"
-	Write-Host "Show Hidden Files: [$($confirmationshowhiddenfiles)]"
-	Write-Host "Allow Remote Desktop: [$($confirmationrdp)]"
-	Write-Host "Allow Wake On LAN: [$($confirmationwol)]"
-	Write-Host "MVPS hosts File: [$($confirmationhostsadb)]"
-} Write-Host
-Write-Host "Windows 10 Setup Script will now run"
-Write-Host
+}
+if ($confirmationchocoinstall -eq "y") {
+	Read-Host "Specify Chocolatey packages to install separeted by spaces"
+}
+while ($confirmationonedrive -ne "n" -and $confirmationonedrive -ne "y") {
+	$confirmationonedrive = Read-Host "Remove all traces of OneDrive? BE CAREFUL [y/n]"
+}
+Write-Output
+Write-Output "Maximum PowerScheme: [$($confirmationpowersch)]"
+Write-Output "App Removal: [$($confirmationappremoval)]"
+Write-Output "Choco install: [$($confirmationchocoinstall)]"
+Write-Output "OneDrive Removal: [$($confirmationonedrive)]"
+Write-Output
+Write-Output "Windows 10 De-Bloat Script will now run"
+Write-Output
 while ($confirmationfull -ne "n" -and $confirmationfull -ne "y") {
- $confirmationfull = Read-Host "Continue? [y/n]"
+	$confirmationfull = Read-Host "Continue? [y/n]"
 } if ($confirmationfull -ne "y") {
- Clear-Host
+	Clear-Host
 	exit
 }
 # Change Windows PowerScheme to maximum performance
 if ($confirmationpowersch -eq "y") {
- $currScheme = powercfg /LIST | Select-String "High performance"
+	$currScheme = powercfg /LIST | Select-String "High performance"
 	$currScheme = $currScheme -split (" ")
 	powercfg -SetActive $currScheme[3]
 }
 # Chocolatey install
 if ($confirmationchocoinstall -eq "y") {
- Write-Host "Installing Chocolatey, specified packages, and all VCRedist Visual C++ versions..." -ForegroundColor yellow
+	Write-Output "Installing Chocolatey, specified packages" -ForegroundColor yellow
 	Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 	choco feature enable -n=allowGlobalConfirmation
 	choco feature disable -n=checksumFiles
@@ -246,49 +192,43 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Phishing
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" -Name "Enabled" -Type DWord -Value 0
 # Remove all Windows store apps except WindowsStore, Calculator Photos and StickyNotes
 if ($confirmationappremoval -eq "y") {
- Write-Host "Removing all Windows store apps except the Windows Store, Calculator, SitckyNotes, and Photos..." -ForegroundColor yellow
+	Write-Output "Removing all Windows store apps except the Windows Store, Calculator, SitckyNotes, and Photos..." -ForegroundColor yellow
 	Get-AppxPackage -AllUsers | Where-Object { $_.Name -notlike "*Microsoft.WindowsStore*" } | Where-Object { $_.Name -notlike "*Microsoft.WindowsCalculator*" } | Where-Object { $_.Name -notlike "*Microsoft.Windows.Photos*" } | Where-Object { $_.Name -notlike "*.NET*" } | Where-Object { $_.Name -notlike "*.VCLibs*" } | Where-Object { $_.Name -notlike "*Sticky*" } | Remove-AppxPackage -ErrorAction 'silentlycontinue'
 	Get-AppxProvisionedPackage -Online | Where-Object { $_.packagename -notlike "*Microsoft.WindowsStore*" } | Where-Object { $_.packagename -notlike "*Microsoft.WindowsCalculator*" } | Where-Object { $_.packagename -notlike "*Microsoft.Windows.Photos*" } | Where-Object { $_.Name -notlike "*.NET*" } | Where-Object { $_.Name -notlike "*.VCLibs*" } | Where-Object { $_.Name -notlike "*Sticky*" } | Remove-AppxProvisionedPackage -Online | Out-Null -ErrorAction 'silentlycontinue'
 }
 # Remove OneDrive
 if ($confirmationonedrive -eq "y") {
- Write-Host "Disabling and removing OneDrive..." -ForegroundColor yellow
+	Write-Output "Disabling and removing OneDrive..." -ForegroundColor yellow
 	taskkill.exe /F /IM "OneDrive.exe"
-	Write-Host "Remove OneDrive..."
+	Write-Output "Remove OneDrive..."
 	if (Test-Path "$env:systemroot\System32\OneDriveSetup.exe") {
 		& "$env:systemroot\System32\OneDriveSetup.exe" /uninstall
 	}
 	if (Test-Path "$env:systemroot\SysWOW64\OneDriveSetup.exe") {
 		& "$env:systemroot\SysWOW64\OneDriveSetup.exe" /uninstall
 	}
-	Write-Host "Disable OneDrive via Group Policies..."
+	Write-Output "Disable OneDrive via Group Policies..."
 	Set-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive" "DisableFileSyncNGSC" 1 -ErrorAction 'silentlycontinue'
-	Write-Host "Removing OneDrive leftovers..."
+	Write-Output "Removing OneDrive leftovers..."
 	Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:localappdata\Microsoft\OneDrive"
 	Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:programdata\Microsoft OneDrive"
 	Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "C:\OneDriveTemp"
-	Write-Host "Removing OneDrive from explorer sidebar..."
+	Write-Output "Removing OneDrive from explorer sidebar..."
 	New-PSDrive -PSProvider "Registry" -Root "HKEY_CLASSES_ROOT" -Name "HKCR"
 	mkdir -Force "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
 	Set-ItemProperty "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" "System.IsPinnedToNameSpaceTree" 0
 	mkdir -Force "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
 	Set-ItemProperty "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" "System.IsPinnedToNameSpaceTree" 0
 	Remove-PSDrive "HKCR"
-	Write-Host "Removing OneDrive run option for new users..."
+	Write-Output "Removing OneDrive run option for new users..."
 	reg load "hku\Default" "C:\Users\Default\NTUSER.DAT"
 }
-# Download MVPS hosts file and backup current hosts file
-if ($confirmationonedrive -eq "y") {
- Write-Host "Backing up hosts file to $($env:SystemRoot)\System32\drivers\etc\hosts.bak" -ForegroundColor yellow
-	Copy-Item "$($env:SystemRoot)\System32\drivers\etc\hosts" -Destination "$($env:SystemRoot)\System32\drivers\etc\hosts.bak"
-	(New-Object Net.WebClient).DownloadString('http://winhelp2002.mvps.org/hosts.txt') | Out-File "$($env:SystemRoot)\System32\drivers\etc\hosts" -Force
-}
-# Finalize
 taskkill /f /im explorer.exe
 Remove-Item 'HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore' -Recurse -Force
 Start-Process explorer.exe
 while ($confirmationreboot -ne "n" -and $confirmationreboot -ne "y") {
- $confirmationreboot = Read-Host "Reboot is recommended reboot this PC now? [y/n]"
+	$confirmationreboot = Read-Host "Reboot is recommended reboot this PC now? [y/n]"
 } if ($confirmationreboot -eq "y") {
- Restart-Computer
-} exit
+	Restart-Computer
+	exit
+}
