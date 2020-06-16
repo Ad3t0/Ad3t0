250aa54a78d56c2a4a62acdcd1a6b379b113a85a
ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2020-06-16 13:49:35 -0600

    Update DomainJoin.ps1

--- powershell-core/DomainJoin.ps1
+++ powershell-core/DomainJoin.ps1
@@ -1,5 +1,8 @@
 $DOMAIN = Read-Host "Enter domain name"
-$DOMAINShort = $DOMAIN.Substring(0, 4).ToUpper()
+if ($DOMAIN.length -gt 4) {
+	$DOMAIN = $DOMAIN.Substring(0, 4)
+}
+$DOMAIN = $DOMAIN.ToUpper()
 $hwInfo = Get-CimInstance -ClassName Win32_ComputerSystem
 $hwInfo = $hwInfo.Manufacturer
 if ($hwInfo.length -gt 4) {
@@ -9,5 +12,5 @@ $hwInfo = $hwInfo.ToUpper()
 $getMac = Get-WmiObject Win32_NetworkAdapter -Filter 'NetConnectionStatus=2'
 $lastOfMac = $getMac.MACAddress -split ":"
 $lastOfMac = "$($lastOfMac[4])$($lastOfMac[5])"
-$pcName = "$($DOMAINShort)-$($hwInfo)-$($lastOfMac)"
-Add-Computer -ComputerName $pcName -DomainName $DOMAIN -Credential Get-Credential
+$pcName = "$($DOMAIN)-$($hwInfo)-$($lastOfMac)"
+Add-Computer -ComputerName $pcName -DomainName $DOMAIN -Credential "Administrator"
