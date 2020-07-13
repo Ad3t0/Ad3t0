2cf719bc076f9622fd8381fed660a9051cc4ba72
ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2020-07-13 12:56:53 -0600

    Update BasicApps.ps1

--- powershell-core/BasicApps.ps1
+++ powershell-core/BasicApps.ps1
@@ -9,10 +9,34 @@ if (!(Test-Path -Path "C:\ProgramData\chocolatey\choco.exe")) {
 	choco feature enable -n=allowGlobalConfirmation
 	choco feature disable -n=checksumFiles
 }
+
 $osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
 if ($osInfo.ProductType -eq 1) {
-	choco install firefox googlechrome vcredist-all dotnetfx directx adobereader
+	choco install firefox googlechrome vcredist-all dotnetfx directx adobereader setdefaultbrowser
+	SetDefaultBrowser.exe HKLM "Firefox-308046B0AF4A39CB"
 }
 else {
-	choco install firefox vcredist-all dotnetfx directx notepadplusplus windirstat
+	""
+	Write-Host "1 - Firefox"
+	Write-Host "2 - Google Chrome"
+	Write-Host "3 - Microsoft Edge"
+	Write-Host "4 - Internet Explorer"
+	""
+	while ($confirmBrowser -ne "1" -and $confirmBrowser -ne "2" -and $confirmBrowser -ne "3" -and $confirmBrowser -ne "4") {
+  $confirmBrowser = Read-Host "Select default browser. [1/2/3/4]"
+	}
+	choco install firefox vcredist-all dotnetfx directx notepadplusplus windirstat setdefaultbrowser
+	if ($confirmBrowser -eq 1) {
+		SetDefaultBrowser.exe HKLM "Firefox-308046B0AF4A39CB"
+	}
+	if ($confirmBrowser -eq 2) {
+		SetDefaultBrowser.exe HKLM "Google Chrome.HEYY3KJOYOAJOPVCHWOFAPMTPI"
+		SetDefaultBrowser.exe HKLM "Google Chrome"
+	}
+	if ($confirmBrowser -eq 3) {
+		SetDefaultBrowser.exe HKLM Edge
+	}
+	if ($confirmBrowser -eq 4) {
+		SetDefaultBrowser.exe HKLM IEXPLORE.EXE
+	}
 }
