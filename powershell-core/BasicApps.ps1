21848c76bbb97abd05574cd2749a71ac0cfdf661
ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2020-08-19 09:14:10 -0600

    Update BasicApps.ps1

--- powershell-core/BasicApps.ps1
+++ powershell-core/BasicApps.ps1
@@ -15,27 +15,5 @@ if ($osInfo.ProductType -ne 1) {
 	SetDefaultBrowser.exe HKLM "Firefox-308046B0AF4A39CB"
 }
 else {
-	""
-	Write-Host "1 - Firefox"
-	Write-Host "2 - Google Chrome"
-	Write-Host "3 - Microsoft Edge"
-	Write-Host "4 - Internet Explorer"
-	""
-	while ($confirmBrowser -ne "1" -and $confirmBrowser -ne "2" -and $confirmBrowser -ne "3" -and $confirmBrowser -ne "4") {
-  $confirmBrowser = Read-Host "Select default browser. [1/2/3/4]"
-	}
-	choco install firefox googlechrome powershell vcredist-all dotnet3.5 dotnetfx directx adobereader setdefaultbrowser
-	if ($confirmBrowser -eq 1) {
-		SetDefaultBrowser.exe HKLM "Firefox-308046B0AF4A39CB"
-	}
-	if ($confirmBrowser -eq 2) {
-		SetDefaultBrowser.exe HKLM "Google Chrome.HEYY3KJOYOAJOPVCHWOFAPMTPI"
-		SetDefaultBrowser.exe HKLM "Google Chrome"
-	}
-	if ($confirmBrowser -eq 3) {
-		SetDefaultBrowser.exe HKLM Edge
-	}
-	if ($confirmBrowser -eq 4) {
-		SetDefaultBrowser.exe HKLM IEXPLORE.EXE
-	}
+	choco install firefox googlechrome powershell vcredist-all dotnet3.5 dotnetfx directx adobereader
 }
