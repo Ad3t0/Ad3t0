245f41f893fd391a3bd855d026be5849856de5e5
ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2020-09-24 08:31:44 -0600

    Update RDPWrapperInstall.ps1

--- powershell-core/RDPWrapperInstall.ps1
+++ powershell-core/RDPWrapperInstall.ps1
@@ -4,26 +4,65 @@ if (!([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups 
     Return
 }
 if (!(Test-Path -Path "C:\Program Files\7-Zip\7z.exe")) {
+    ""
+    Write-Host "Downloading and installing 7zip..."
+    ""
     $url = "https://www.7-zip.org/a/7z1900-x64.exe"
     $output = "$($env:TEMP)\7z1900-x64.exe"
     Invoke-WebRequest -Uri $url -OutFile $output
     ."$($env:TEMP)\7z1900-x64.exe" /S
     Wait-Process -Name 7z1900-x64
 }
+""
+Write-Host "Adding Windows Defender exclusions..."
+""
 Add-MpPreference -ExclusionPath "$($env:TEMP)\RDPWrap-v1.6.2.zip"
 Add-MpPreference -ExclusionPath "$($env:TEMP)\autoupdate.zip"
 Add-MpPreference -ExclusionPath "C:\Program Files\RDP Wrapper\"
 Add-MpPreference -ExclusionPath "C:\Program Files\RDP Wrapper\rdpwrap.dll"
 Add-MpPreference -ExclusionProcess rdpwrap.dll
+Start-Sleep 2
+""
+Write-Host "Downloading RDPWrap-v1.6.2.zip..."
+""
 $url = "https://github.com/stascorp/rdpwrap/releases/download/v1.6.2/RDPWrap-v1.6.2.zip"
 $output = "$($env:TEMP)\RDPWrap-v1.6.2.zip"
 Invoke-WebRequest -Uri $url -OutFile $output
+""
+Write-Host "Downloading autoupdate.zip..."
+""
 $url = "https://github.com/asmtron/rdpwrap/raw/master/autoupdate.zip"
 $output = "$($env:TEMP)\autoupdate.zip"
 Invoke-WebRequest -Uri $url -OutFile $output
+""
+Write-Host "Extracting RDPWrap-v1.6.2.zip..."
+""
 ."C:\Program Files\7-Zip\7z.exe" x "$($env:TEMP)\RDPWrap-v1.6.2.zip" -o"C:\Program Files\RDP Wrapper"
+""
+Write-Host "Extracting autoupdate.zip..."
+""
 ."C:\Program Files\7-Zip\7z.exe" x "$($env:TEMP)\autoupdate.zip" -o"C:\Program Files\RDP Wrapper"
+""
+Write-Host "Installing RDPWrapper..."
+""
 ."C:\Program Files\RDP Wrapper\RDPWInst.exe" -i -o
+""
+Write-Host "Updating RDPWrapper..."
+""
 cmd /c "C:\Program Files\RDP Wrapper\autoupdate.bat"
-Stop-Service wuauserv
-Set-Service wuauserv -StartupType Disabled
+while ($disableUpdatesConfirm -ne "n" -and $disableUpdatesConfirm -ne "y") {
+    $disableUpdatesConfirm = Read-Host "Disable Windows Updates? [y/n]"
+}
+if ($disableUpdatesConfirm -eq "y") {
+    ""
+    Write-Host "Stopping Windows Update Service..."
+    ""
+    Stop-Service wuauserv
+    ""
+    Write-Host "Disabling Windows Update Service..."
+    ""
+    Set-Service wuauserv -StartupType Disabled
+}
+""
+Write-Host "RDPWrapper Install Complete..."
+""
