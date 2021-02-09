46bb067c5f161806d565e9c5289383c5c6050dd1
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2021-02-08 20:24:11 -0700

    Create AutoLogin.ps1

---
+++ powershell-core/AutoLogin.ps1
@@ -0,0 +1,27 @@
+while (!($verifiedCreds)) {
+    $username = Read-Host "Enter the autologin username"
+    $password = Read-Host "Enter the autologin password"
+    $computer = $env:COMPUTERNAME
+    Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+    $obj = New-Object System.DirectoryServices.AccountManagement.PrincipalContext ('machine', $computer)
+    $goodCreds = $obj.ValidateCredentials($username, $password)
+    if ($goodCreds) {
+        $verifiedCreds = $True
+    }
+    else {
+        ""
+        Write-Warning "Credentials failed to validate"
+        ""
+    }
+}
+$username = $username -split "\\"
+if ($username.Count -eq 2) {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultDomainName" -Value $username[0]
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultUserName" -Value $username[0]
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultPassword" -Value $username[0]
+}
+else {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultUserName" -Value $username
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultPassword" -Value $password
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "AutoAdminLogon" -Value 1
