c3da58ca30aea2e719f4e5a93300d2865ed20185
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2021-06-01 15:30:03 -0600

    update

--- powershell-core/WinUpdate.ps1
+++ powershell-core/WinUpdate.ps1
@@ -2,25 +2,29 @@ while ($fileTempConf -ne "n" -and $fileTempConf -ne "y") {
     ""
     $fileTempConf = Read-Host "Install all Windows updates rebooting automatically untill all are complete? [y/n]"
 }
-
-
-
-
-
-
-$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "C:\ProgramData\PSS3B-Data\PSS3B-Task.ps1"
-$Trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek $COMBOBOX_Day.text -At $TEXTBOX_Time.text
+$taskFile = @'
+Import-Module PSWindowsUpdate
+$updates = Get-WUInstall -AcceptAll -AutoReboot
+Install-WindowsUpdate -AcceptAll -AutoReboot
+if (!($updates)) {
+    schtasks.exe /delete /tn WinUpdate /f
+    Remove-Item -Path "C:\ProgramData\WinUpdate.ps1" -Force
+}
+shutdown /r /t 0
+'@
+Set-Content "C:\ProgramData\WinUpdate.ps1" $taskFile
+$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-file C:\ProgramData\WinUpdate.ps1"
+$Trigger = New-ScheduledTaskTrigger -AtStartup
 $Settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit 0
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
-Register-ScheduledTask -TaskName 'PSS3B_Run' -InputObject $Task -User $TEXTBOX_Username.text -Password $TEXTBOX_Password.text
-
-
-
-
-
-
+Register-ScheduledTask -TaskName 'WinUpdate' -InputObject $Task -User SYSTEM
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSWindowsUpdate -Confirm:$False -Force
 Import-Module PSWindowsUpdate
-Get-WUInstall -AcceptAll -AutoReboot
+$updates = Get-WUInstall -AcceptAll -AutoReboot
 Install-WindowsUpdate -AcceptAll -AutoReboot
+if (!($updates)) {
+    schtasks.exe /delete /tn WinUpdate /f
+    Remove-Item -Path "C:\ProgramData\WinUpdate.ps1" -Force
+}
+shutdown /r /t 0
