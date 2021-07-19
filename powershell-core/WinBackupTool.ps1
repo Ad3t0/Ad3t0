dd12fe50860a9031fadba51df209fd6b82446ad9
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2021-07-18 22:26:22 -0600

    Update WinBackupTool.ps1

--- powershell-core/WinBackupTool.ps1
+++ powershell-core/WinBackupTool.ps1
@@ -1,32 +1,16 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $logObj = [PSCustomObject]@{
-    backupName = $null
-    backupPath = $null
+    backupName            = $null
+    backupSourcePath      = $null
+    backupDestinationPath = $null
+    backupTime            = $null
+    backupRetention       = $null
 }
-
-
-
-
-
 $logObj.backupName = Read-Host "Enter name for backup"
-
-
-
-
-
-$pathToJson = "C:\ProgramData\WinBackup\WinBackup.json"
-
-Set-Content $pathToJson $defaultSettings
-
-$logObj | ConvertTo-Json | Set-Content $pathToJson
-
-
-
-
-
-$timeScriptRun = Get-Date -UFormat '+%Y-%m-%dT%H-%M-%S'
-
-
+$logObj.backupSourcePath = Read-Host "Enter a source path to backup"
+$logObj.backupDestinationPath = Read-Host "Enter a destination path to backup to"
+$logObj.backupTime = Read-Host "Enter a time to backup, formatted like 3am for 3:00 AM"
+$logObj.backupRetention = Read-Host "How many backups to keep"
 if (!(Test-Path -Path "C:\Program Files\7-Zip\7z.exe")) {
     $url = "https://www.7-zip.org/a/7z1900-x64.exe"
     $output = "$($env:TEMP)\7z1900-x64.exe"
@@ -34,41 +18,28 @@ if (!(Test-Path -Path "C:\Program Files\7-Zip\7z.exe")) {
     ."$($env:TEMP)\7z1900-x64.exe" /S
     Wait-Process -Name 7z1900-x64
 }
-
-
-if (!(Test-Path -Path "C:\ProgramData\WinUpdate")) {
-    New-Item -Path "C:\ProgramData\WinUpdate" -ItemType "directory"
+if (!(Test-Path -Path "C:\ProgramData\WinBackup")) {
+    New-Item -Path "C:\ProgramData\WinBackup" -ItemType "directory"
 }
-
-
-
-
+if (!(Test-Path -Path $logObj.backupDestinationPath)) {
+    New-Item -Path $logObj.backupDestinationPath -ItemType "directory" -Force
+}
+$pathToJson = "C:\ProgramData\WinBackup\WinBackup_$($logObj.backupName).json"
+$logObj | ConvertTo-Json | Set-Content $pathToJson
 $taskFile = @'
-    $pathToJson = "C:\ProgramData\WinUpdate\WinUpdate.json"
-    $jsonSettings = Get-Content -Path $pathToJson -Raw | ConvertFrom-Json
-    $jsonSettings.rebootCount = [int]$jsonSettings.rebootCount
-Import-Module PSWindowsUpdate
-$updates = Get-WUInstall -AcceptAll -AutoReboot -SendHistory | Format-List | Out-String | Add-Content "C:\ProgramData\WinUpdate\$($timeScriptRun).txt"
-Install-WindowsUpdate -AcceptAll -AutoReboot -SendHistory | Format-List | Out-String |  Add-Content "C:\ProgramData\WinUpdate\$($timeScriptRun).txt"
-if (!($updates) -or $jsonSettings.rebootCount -ge 6) {
-    schtasks.exe /delete /tn WinUpdate /f
-    Remove-Item -Path "C:\ProgramData\WinUpdate\WinUpdate.ps1" -Force
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "legalnoticecaption" -Value ""
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "legalnoticetext" -Value ""
+param ($backupName)
+$pathToJson = "C:\ProgramData\WinBackup\WinBackup_$backupName.json"
+$logObj = Get-Content -Path $pathToJson -Raw | ConvertFrom-Json
+$timeScriptRun = Get-Date -UFormat '+%Y-%m-%dT%H-%M-%S'
+$backupCount = Get-ChildItem $logObj.backupDestinationPath
+if ($backupCount.Count -gt [int]$logObj.backupRetention) {
+    $purgeBackup = Get-ChildItem $logObj.backupDestinationPath | Where-Object Name -Like "WinBackup_$($backupName)_*.7z" |Sort-Object LastWriteTime -Descending | Select-Object -Last 1
+    Remove-Item $purgeBackup.VersionInfo.FileName
 }
-$jsonSettings.rebootCount = $jsonSettings.rebootCount + 1
-$jsonSettings | ConvertTo-Json | Set-Content $pathToJson
-shutdown /r /t 0 /f
+."C:\Program Files\7-Zip\7z.exe" a -t7z "$($logObj.backupDestinationPath)\WinBackup_$($logObj.backupName)_$($timeScriptRun).7z" $logObj.backupSourcePath -mx9
 '@
-Set-Content "C:\ProgramData\WinUpdate\WinUpdate.ps1" $taskFile
-
-
-
-$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-NoLogo -WindowStyle Hidden -ExecutionPolicy Bypass -File C:\ProgramData\WinUpdate\WinUpdate.ps1"
-$Trigger = New-ScheduledTaskTrigger -AtStartup
+Set-Content "C:\ProgramData\WinBackup\WinBackup_$($logObj.backupName).ps1" $taskFile
+$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-NoLogo -WindowStyle Hidden -ExecutionPolicy Bypass -File C:\ProgramData\WinBackup\WinBackup_$($logObj.backupName).ps1 -backupName $($logObj.backupName)"
+$Trigger = New-ScheduledTaskTrigger -Daily -At $logObj.backupTime
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger
-Register-ScheduledTask -TaskName 'WinUpdate' -InputObject $Task -User SYSTEM
-
-
-
-
+Register-ScheduledTask -TaskName "WinBackup_$($logObj.backupName)" -InputObject $Task -User SYSTEM
