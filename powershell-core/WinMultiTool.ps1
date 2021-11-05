50d653774bce6289f0d58d4eeb6c771fe0922b2d
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2021-11-05 13:13:38 -0600

    Update WinMultiTool.ps1

--- powershell-core/WinMultiTool.ps1
+++ powershell-core/WinMultiTool.ps1
@@ -38,9 +38,14 @@ if ($functionsToRun -like "*1*") {
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
         choco feature enable -n allowGlobalConfirmation
+        choco feature enable -n skipPackageUpgradesWhenNotInstalled
         choco feature disable -n checksumFiles
+
     }
     else {
+        choco feature enable -n allowGlobalConfirmation
+        choco feature enable -n skipPackageUpgradesWhenNotInstalled
+        choco feature disable -n checksumFiles
         choco upgrade chocolatey
         choco upgrade all
     }
