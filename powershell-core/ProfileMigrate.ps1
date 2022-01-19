5ebec8d37ce0ab975aeb8cef1ee69f43eec73175
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2022-01-19 16:53:13 -0700

    Update ProfileMigrate.ps1

--- powershell-core/ProfileMigrate.ps1
+++ powershell-core/ProfileMigrate.ps1
@@ -12,10 +12,10 @@ Function Get-Folder($initialDirectory = "") {
 $refName = Read-Host "Enter migration name for reference"
 $migratePath = Get-Folder
 $documentsPath = [Environment]::GetFolderPath("MyDocuments")
-$downloadsPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-$picturesPath = [Environment]::GetFolderPath("MyPictures")
 $desktopPath = [Environment]::GetFolderPath("Desktop")
-robocopy $documentsPath "$($migratePath)\$($refName)\Documents" /s /np /eta /xf *.lnk *.pst *.exe desktop.ini | Write-Host
-robocopy $desktopPath "$($migratePath)\$($refName)\Desktop" /s /np /eta /xf *.lnk *.pst *.exe desktop.ini | Write-Host
-robocopy $picturesPath "$($migratePath)\$($refName)\Pictures" /s /np /eta /xf *.lnk *.pst *.exe desktop.ini | Write-Host
-robocopy $downloadsPath "$($migratePath)\$($refName)\Downloads" /s /np /eta /xf *.lnk *.pst *.exe desktop.ini | Write-Host
+$picturesPath = [Environment]::GetFolderPath("MyPictures")
+$downloadsPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+robocopy $documentsPath "$($migratePath)\$($refName)\Documents" /s /np /eta /xf *.lnk *.pst *.exe *.dll desktop.ini | Write-Host
+robocopy $desktopPath "$($migratePath)\$($refName)\Desktop" /s /np /eta /xf *.lnk *.pst *.exe *.dll desktop.ini | Write-Host
+robocopy $picturesPath "$($migratePath)\$($refName)\Pictures" /s /np /eta /xf *.lnk *.pst *.exe *.dll desktop.ini | Write-Host
+robocopy $downloadsPath "$($migratePath)\$($refName)\Downloads" /s /np /eta /xf *.lnk *.pst *.exe *.dll desktop.ini | Write-Host
