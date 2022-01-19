07c6bc60bba387275ad56a6518167d271bbd181d
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2022-01-19 11:42:54 -0700

    Update ProfileMigrate.ps1

--- powershell-core/ProfileMigrate.ps1
+++ powershell-core/ProfileMigrate.ps1
@@ -1,16 +1,23 @@
-$computer = $env:COMPUTERNAME
-$users = C:\Windows\System32\query.exe user /server:$computer 2>&1
-$users = $users | ForEach-Object {
+$edition = Get-ComputerInfo | Select-Object WindowsProductName
+if ($edition.WindowsProductName -like "*Pro*") {
+	$computer = $env:COMPUTERNAME
+	$users = C:\Windows\System32\query.exe user /server:$computer 2>&1
+	$users = $users | ForEach-Object {
     (($_.trim() -replace ">" -replace "(?m)^([A-Za-z0-9]{3,})\s+(\d{1,2}\s+\w+)", '$1  none  $2' -replace "\s{2,}", "," -replace "none", $null))
-} | ConvertFrom-Csv
-foreach ($user in $users) {
-	if ($user.STATE -eq "Active") {
-		$currentUser = $user.USERNAME
+	} | ConvertFrom-Csv
+	foreach ($user in $users) {
+		if ($user.STATE -eq "Active") {
+			$currentUser = $user.USERNAME
+		}
 	}
+	$profilePath = "C:\Users\$($currentUser)"
 }
-$profilePath = "C:\Users\$($currentUser)"
-Function Get-Folder($initialDirectory = "")
-{
+else {
+	$user = (Get-WMIObject -ClassName Win32_ComputerSystem).Username
+	$currentUser = $user.Split("\")
+	$profilePath = "C:\Users\$($currentUser[1])"
+}
+Function Get-Folder($initialDirectory = "") {
 	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 	$folderName = New-Object System.Windows.Forms.FolderBrowserDialog
 	$folderName.Description = "Select a folder"
