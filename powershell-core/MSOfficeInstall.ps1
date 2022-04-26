c69a0477a12b3ef1169334a81d2cfe857336f255
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2022-04-26 12:07:56 -0600

    update

--- powershell-core/MSOfficeInstall.ps1
+++ powershell-core/MSOfficeInstall.ps1
@@ -27,11 +27,11 @@ Function Hide-PowerShellWindow() {
 	}
 }
 if (!(Test-Path -Path "C:\Program Files\7-Zip\7z.exe")) {
-	$url = "https://www.7-zip.org/a/7z1900-x64.exe"
-	$output = "$($env:TEMP)\7z1900-x64.exe"
-	Invoke-WebRequest -Uri $url -OutFile $output
-	."$($env:TEMP)\7z1900-x64.exe" /S
-	Wait-Process -Name 7z1900-x64
+    $url = "https://www.7-zip.org/a/7z2107-x64.exe"
+    $output = "$($env:TEMP)\7z2107-x64.exe"
+    Invoke-WebRequest -Uri $url -OutFile $output
+    ."$($env:TEMP)\7z2107-x64" /S
+    Wait-Process -Name 7z2107-x64
 }
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_12827-20268.exe"
 $output = "$($env:TEMP)\officedeploymenttool_12827-20268.exe"
--- powershell-core/WinX.ps1
+++ powershell-core/WinX.ps1
@@ -1,11 +1,12 @@
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Copy-Item -Path "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Destination "C:\Windows\System32\WindowsPowerShell\v1.0\powershellWinX.exe"
 if (!(Test-Path -Path "C:\Program Files\7-Zip\7z.exe")) {
-    $url = "https://www.7-zip.org/a/7z1900-x64.exe"
-    $output = "$($env:TEMP)\7z1900-x64.exe"
+    $url = "https://www.7-zip.org/a/7z2107-x64.exe"
+    $output = "$($env:TEMP)\7z2107-x64.exe"
     Invoke-WebRequest -Uri $url -OutFile $output
-    ."$($env:TEMP)\7z1900-x64.exe" /S
-    Wait-Process -Name 7z1900-x64
+    ."$($env:TEMP)\7z2107-x64" /S
+    Wait-Process -Name 7z2107-x64
 }
 $url = "https://github.com/Beej126/WinXMenuPwsh/archive/refs/heads/main.zip"
 $output = "$($env:TEMP)\main.zip"
