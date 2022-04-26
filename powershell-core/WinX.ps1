83f58a30b81a73035d8fe2cf8d2f4be3045208df
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2022-04-26 13:08:46 -0600

    Update WinX.ps1

--- powershell-core/WinX.ps1
+++ powershell-core/WinX.ps1
@@ -7,3 +7,15 @@ if (!(Test-Path -Path "C:\Program Files\7-Zip\7z.exe")) {
     ."$($env:TEMP)\7z2107-x64" /S
     Wait-Process -Name 7z2107-x64
 }
+Remove-Item -Path "$($env:TEMP)\Group3.7z" -Force -ErrorAction SilentlyContinue
+$url = "https://github.com/Ad3t0/windows/raw/master/bin/Group3.7z"
+$output = "$($env:TEMP)\Group3.7z"
+Invoke-WebRequest -Uri $url -OutFile $output
+."C:\Program Files\7-Zip\7z.exe" x "$($env:TEMP)\Group3.7z" -o"$($env:TEMP)" -aoa
+$userCheck = Get-ChildItem -LiteralPath "C:\Users"
+foreach ($item in $userCheck.Name) {
+    if ($item -ne "Public") {
+        Remove-Item -Path "C:\Users\$($item)\AppData\Local\Microsoft\Windows\WinX\Group3" -Recurse -Force
+        Copy-Item -Path "$($env:TEMP)\Group3" -Destination "C:\Users\$($item)\AppData\Local\Microsoft\Windows\WinX\" -Recurse -Force
+    }
+}
