69042bd1a74dec1f6c3f7d6c001cdd2405642b08
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2023-01-27 11:34:00 -0700

    update

--- powershell-core/RepairTrust.ps1
+++
@@ -1,5 +0,0 @@
-$username = Read-Host "Enter the username for renaming"
-$password = Read-Host "Enter the domain user password for renaming"
-$encrypted = ConvertTo-SecureString $password -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PsCredential($username, $encrypted)
-Test-ComputerSecureChannel -Repair -Credential $credential
