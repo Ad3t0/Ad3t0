8e61b1925539ff6cc1cb843ce78cbfd7aa004c22
Ad3t0 <ad3t000@gmail.com>
2025-06-12 17:36:22 +0000

    windows update script changes

--- powershell/InstallUpdates.ps1
+++ powershell/InstallUpdates.ps1
@@ -1,18 +1,7 @@
-# Settings
-$msUpdateServiceID = "7971f918-a847-4430-9279-4a52d1efe18d"
-$excludedUpdates = "'Teams', 'OneDrive'"
-$updateCriteria = "isinstalled=0 and deploymentaction=*"
-
-# Install required providers and modules
-$null = Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-$null = Install-Module -Name PSWindowsUpdate -Force
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name PSWindowsUpdate -Confirm:$False -Force
 Import-Module PSWindowsUpdate
-
-# Wait for module to load
-Start-Sleep -Seconds 5
-
-# Add Microsoft Update Service
-Add-WUServiceManager -ServiceID $msUpdateServiceID -AddServiceFlag 7 -Confirm:$false
-
-# Install Windows Updates
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -NotTitle $excludedUpdates -Criteria $updateCriteria
+Start-Sleep 5
+Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
+Start-Sleep 30
+Get-WindowsUpdate -MicrosoftUpdate -Install -AcceptAll -AutoReboot -Verbose
